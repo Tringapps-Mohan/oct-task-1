638d3bdbe2098916e6fe2b0973dcd8398e2b1966
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2022-10-19 16:26:44 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 function Card(props){
     return (
         <div className="container">
-            {Object.entries(props).map(e=><div>{e[0].toUpperCase()}:{e[1]}</div>)}
+            {Object.entries(props).map(e=><div><span className="titles">{e[0].toUpperCase()}</span>:<span className="values">{e[1]}</span></div>)}
         </div>
     )
 }
